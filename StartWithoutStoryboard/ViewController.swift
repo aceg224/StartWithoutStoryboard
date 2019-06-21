6be12a252a1cf8f741fb882e1b9f82c7a16eec80
aceg224 <aceg224@gmail.com>
2019-06-21 19:52:01 +0800

    Initial Commit

---
+++ StartWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartWithoutStoryboard
+//
+//  Created by min-chia on 2019/6/21.
+//  Copyright © 2019 min-chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
